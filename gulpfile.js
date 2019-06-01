5a46d07c67ce67abf51f8839281edc4011cf5bff
rob-kistner <robkistner@mac.com>
2019-06-01 09:42:23 -0500

    committing for upload

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const { src, dest, watch, parallel, series } = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync').create()
+
+const
+  html_src =   'src/html/*.html',
+  html_dest =  'dist/',
+  scss_src =   'src/scss/*.scss',
+  scss_dest =  'dist/css',
+  scss_watch = 'src/scss/**/*.s(a|c)ss'
+  ;
+
+function html() {
+  return src(html_src)
+    .pipe(dest(html_dest))
+    .pipe(browserSync.stream())
+}
+
+function scss() {
+  return src(scss_src)
+    .pipe(sass({outputStyle: 'expanded'}))
+      .on("error", sass.logError)
+    .pipe(dest(scss_dest))
+    .pipe(browserSync.stream())
+}
+
+
+function watchers() {
+  
+  browserSync.init({
+    server: {
+      baseDir: 'dist',
+    },
+    ui: {
+      port: 8080
+    },
+    notify: false
+  })
+
+  watch(html_src, html)
+  watch(scss_watch, scss)
+  watch('dist/**/*.*').on('change', browserSync.reload)
+}
+
+exports.html       = html
+exports.scss       = scss
+exports.watchers   = watchers
+
+exports.default = series(parallel(scss, html), watchers)
